92ab83e5bfcbf27b2e1168e04a03e4e9f195cae4
islammedmykindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2022-07-19 00:54:48 -0400

    The bot can now be launched with an existing token if configured to true
    removed the cool chunking code... :(

--- botGoals.md
+++ botGoals.md
@@ -1,15 +1,17 @@
 # Goals for this bot
 
-* make a system that goes over the 500 character twitch message limit. [...]
+* ~~make a system that goes over the 500 character twitch message limit. [...]~~ ...tmi.js already can do that -_-
 * exception handling
 * slash command settings
-* manage messages, both from twitch and discord
+* manage [delete, edit] messages, both from twitch and discord
     * challenge: if a message is > 500, how would editing large messages be handled...
 * replies from discord are relfected on twitch
+    * may not be technically possible in the way one would normally expect.
 * indicate custom emoji
 * refresh the token...
-* create a system for devs that stores the refresh token in a temporary spot.
+* ~~create a system for devs that stores the refresh token in a temporary spot.~~
 * create a channel template that reflects the limitations of twitch
     * setup a guideline for cross-custom emoji between discord and twitch
-* use jsDoc to document the function
+* use jsDoc to document the functions
 * see what happens if someone types a [twitch] command from discord into twitch
+    * Because the bot alredy creates a minimal header for discord and twitch messages, it's not completely necessary. However, if the message overflows past 500 characters, there may be a way to exploit this, so we need filters for that somehow
--- twitchToDiscord.js
+++ twitchToDiscord.js
@@ -5,7 +5,8 @@
 const configFile = require('./config.json'),
     tmijs = require('tmi.js'),
     { authenticateTwitch } = require('./oauth'),
-    { Client } = require('discord.js');
+    { Client } = require('discord.js'),
+    fs = require('fs');
 
 var twitchClient; //Defined after retrieving the authentication token.
 
@@ -15,21 +16,23 @@ const genericPromiseError =  error=>console.error('Snap, I hit a snag... >.<', e
 //Twitch init
 /////////////
 
-//By-golly this initialization code is much larger than discord.js o_O
+/**
+ * Login to twitch using the access token found in our oauth process
+ * @param {string} {access_token} a javascript object containing at minimum, a twitch access token 
+ */
+function loginToTwitch({access_token}, skipTokenSave){
 
-//Login to twitch
-//There are a lot of things that were named differently because this command has the potential of using environemnt variables, so this will be painful to look at and I'm sorry XP
-authenticateTwitch({
-    scope: configFile.T2S_SCOPE,
-    redirect_uri: configFile.T2S_REDIRECT_URI,
-    client_id: configFile.T2S_CLIENT_ID,
-    client_secret: configFile.T2S_SECRET
-}).then(resJson=>{
+    //If we have the save token flag enabled, save this to "devTwitchToken"
+    if(!skipTokenSave && configFile.T2S_DEV_SAVE_TOKEN)
+        fs.writeFile('./devTwitchToken', access_token, undefined, error=>{
+            if(error) console.error("I hit a snag...", error);
+        });
+
     twitchClient = tmijs.Client({
         options: {debug:true},
         identity:{
             username:configFile.T2S_USER,
-            password:'oauth:'+resJson.access_token
+            password:'oauth:'+access_token
         },
         channels: configFile.T2S_CHANNELS
     });
@@ -40,54 +43,34 @@ authenticateTwitch({
         //Something fascinating is, if a message is sent by the bot, self will be true. If I use the same username though when I chat, it's false.
         if(!self) targetDiscordChannel.send(`[t][${userState["display-name"]}] ${msg}`).then(undefined, genericPromiseError);
     })  
-})
+}
+
+//Login to twitch
+//There are a lot of things that were named differently because this command has the potential of using environemnt variables, so this will be painful to look at and I'm sorry XP
+if(configFile.T2S_DEV_SAVE_TOKEN && fs.existsSync('./devTwitchToken')){
+    //If the file exists, use the thing
+    console.log('==Found the token file! (devTwitchToken!) Using that instead of logging in via oauth (if this token is old, delete the file)==');
+    loginToTwitch({access_token: fs.readFileSync('./devTwitchToken')}, true);
+}
+
+else authenticateTwitch({
+    scope: configFile.T2S_SCOPE,
+    redirect_uri: configFile.T2S_REDIRECT_URI,
+    client_id: configFile.T2S_CLIENT_ID,
+    client_secret: configFile.T2S_SECRET
+}).then(loginToTwitch);
 
 //discord.js init
 /////////////////
 const discordClient = new Client({intents: ['GUILDS', 'GUILD_MESSAGES']});
 var targetDiscordChannel;
 
-function chunkMessage(message = "", contSymbol = "[...]"){
-    const res = [""];
-    var resIndex = 0;
-
-    for(let i = 0; i < message.length; i++){
-
-        if(res[resIndex].length == 500){
-            resIndex++;
-            res[resIndex] = "";
-        }
-
-        //If our current chunk comes after the first, and the current string is blank, add the continue symbol
-        if(resIndex && !res[resIndex]) res[resIndex] += contSymbol;
-
-        res[resIndex] += message[i];
-    }
-
-    return res;
-}
-
 discordClient.on('messageCreate', m=>{
     //Ping it back to twitch as long as it isn't a bot
     if(!m.author.bot && m.channel.id == targetDiscordChannel.id){
         //Break the message down if it is over 500 characters to send things over from discord
         const discordHeader = `[d][${m.author.tag}] `;
-        if(m.content.length > (500 - discordHeader.length)) {
-            const messagesToSend = chunkMessage(discordHeader + m.content);
-            let currIndex = 0;
-            const recursiveSend = ()=>{
-                if(currIndex < messagesToSend.length)
-                twitchClient.say(configFile.T2S_CHANNELS[0], messagesToSend[currIndex]).then(()=>{
-                    currIndex++;
-                    // setTimeout(recursiveSend, 500);
-                    recursiveSend();
-                });
-            }
-
-            recursiveSend();
-        }
-
-        else twitchClient.say(configFile.T2S_CHANNELS[0], `${discordHeader}${m.content}`).then(undefined, genericPromiseError);
+        twitchClient.say(configFile.T2S_CHANNELS[0], `${discordHeader}${m.content}`).then(undefined, genericPromiseError);
     }
 });
 
