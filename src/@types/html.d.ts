6e2b1808efb307511c11c3f768652bd7f64cded7
AceLikesGhosts <acelikesghosts@gmail.com>
2022-09-24 13:35:05 -0700

    (RM): html.d.ts

--- src/@types/html.d.ts
+++
@@ -1,5 +0,0 @@
-// Declaring a module with *.html due to us
-// not having a default import w/ html
-// if we define properties / export a default it'll
-// compile to `require('thing.html').default` lol
-declare module '*.html';
